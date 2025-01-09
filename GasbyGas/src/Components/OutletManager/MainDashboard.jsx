caacb4d89a83ec969aed365f528c1b515a8128d7
krishan44 <124803438+krishan44@users.noreply.github.com>
2025-01-09 22:55:29 +0530

    Enhance MainDashboard layout and styling; add new SVG assets for improved UI

--- GasbyGas/src/Components/OutletManager/MainDashboard.jsx
+++ GasbyGas/src/Components/OutletManager/MainDashboard.jsx
@@ -1,14 +1,23 @@
 import style from "./MainDashboard.module.css"
+import order from "../../assets/Pannel/order.svg";
 
 function MainDashboard(){
     return(
         <>
             <div className={style.Maindashboard}>
-                <div className={style.panel}>
-                    <div className={style.logo}>GasbyGas</div>
-                </div>
-                <div className={style.routes}>
-                    <span>overview</span>
+                <div className={style.sidePannel}>
+                    <div className={style.head}>
+                        <div className={style.logo}>GasbyGas</div>
+                        <span>Outlet</span>
+                    </div>
+                    <div className={style.routes}>
+                        <span className={`${style.option} ${style.selected}`}>Overview</span>
+                        <span className={style.option}>Customer Orders</span>
+                        <span className={style.option}>Business Orders</span>
+                        <span className={style.option}>Stock Orders</span>
+                        <span className={style.option}>Waitlist Manage</span>
+                        <span className={style.option}>Account Manage</span>
+                    </div>
                 </div>
             </div>
         </>
