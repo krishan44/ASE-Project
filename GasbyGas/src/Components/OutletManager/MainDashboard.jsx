1fc2cab8a03328603763178361cf5df8c4f60649
krishan44 <124803438+krishan44@users.noreply.github.com>
2025-01-09 23:23:33 +0530

    Refactor MainDashboard component to include icons for menu options and improve layout styling

--- GasbyGas/src/Components/OutletManager/MainDashboard.jsx
+++ GasbyGas/src/Components/OutletManager/MainDashboard.jsx
@@ -11,12 +11,30 @@ function MainDashboard(){
                         <span>Outlet</span>
                     </div>
                     <div className={style.routes}>
-                        <span className={`${style.option} ${style.selected}`}>Overview</span>
-                        <span className={style.option}>Customer Orders</span>
-                        <span className={style.option}>Business Orders</span>
-                        <span className={style.option}>Stock Orders</span>
-                        <span className={style.option}>Waitlist Manage</span>
-                        <span className={style.option}>Account Manage</span>
+                        <div className={style.optionContainer}>
+                            <img src={order} alt="" className={style.panelIcon}/>
+                            <span className={`${style.option} ${style.selected}`}>Overview</span>
+                        </div>
+                        <div className={style.optionContainer}>
+                            <img src={order} alt="" className={style.panelIcon}/>
+                            <span className={style.option}>Customer Orders</span>
+                        </div>
+                        <div className={style.optionContainer}>
+                            <img src={order} alt="" className={style.panelIcon}/>
+                            <span className={style.option}>Business Orders</span>
+                        </div>
+                        <div className={style.optionContainer}>
+                            <img src={order} alt="" className={style.panelIcon}/>
+                            <span className={style.option}>Stock Orders</span>
+                        </div>
+                        <div className={style.optionContainer}>
+                            <img src={order} alt="" className={style.panelIcon}/>
+                            <span className={style.option}>Waitlist Manage</span>
+                        </div>
+                        <div className={style.optionContainer}>
+                            <img src={order} alt="" className={style.panelIcon}/>
+                            <span className={style.option}>Account Manage</span>
+                        </div>
                     </div>
                 </div>
             </div>
