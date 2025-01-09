d95b22fb9ce3cd6085d089143dd57b9efa30753d
krishan44 <124803438+krishan44@users.noreply.github.com>
2025-01-09 23:56:42 +0530

    Add new SVG icons and update MainDashboard component for improved navigation

--- GasbyGas/src/Components/OutletManager/MainDashboard.jsx
+++ GasbyGas/src/Components/OutletManager/MainDashboard.jsx
@@ -1,5 +1,11 @@
 import style from "./MainDashboard.module.css"
 import order from "../../assets/Pannel/order.svg";
+import Customer from "../../assets/Pannel/customer.svg";
+import dashboard from "../../assets/Pannel/dashboard.svg";
+import report from "../../assets/Pannel/report.svg";
+import stock from "../../assets/Pannel/stock.svg";
+import orderstock from "../../assets/Pannel/orderstock.svg";
+import waitlist from "../../assets/Pannel/waitlist.svg";
 
 function MainDashboard(){
     return(
@@ -12,7 +18,7 @@ function MainDashboard(){
                     </div>
                     <div className={style.routes}>
                         <div className={style.optionContainer}>
-                            <img src={order} alt="" className={style.panelIcon}/>
+                            <img src={dashboard} alt="" className={style.panelIcon}/>
                             <span className={`${style.option} ${style.selected}`}>Overview</span>
                         </div>
                         <div className={style.optionContainer}>
@@ -20,20 +26,24 @@ function MainDashboard(){
                             <span className={style.option}>Customer Orders</span>
                         </div>
                         <div className={style.optionContainer}>
-                            <img src={order} alt="" className={style.panelIcon}/>
+                            <img src={orderstock} alt="" className={style.panelIcon}/>
                             <span className={style.option}>Business Orders</span>
                         </div>
                         <div className={style.optionContainer}>
-                            <img src={order} alt="" className={style.panelIcon}/>
+                            <img src={stock} alt="" className={style.panelIcon}/>
                             <span className={style.option}>Stock Orders</span>
                         </div>
                         <div className={style.optionContainer}>
-                            <img src={order} alt="" className={style.panelIcon}/>
+                            <img src={waitlist} alt="" className={style.panelIcon}/>
                             <span className={style.option}>Waitlist Manage</span>
                         </div>
                         <div className={style.optionContainer}>
-                            <img src={order} alt="" className={style.panelIcon}/>
+                            <img src={Customer} alt="" className={style.panelIcon}/>
                             <span className={style.option}>Account Manage</span>
+                        </div>
+                        <div className={style.optionContainer}>
+                            <img src={report} alt="" className={style.panelIcon}/>
+                            <span className={style.option}>Report Generation</span>
                         </div>
                     </div>
                 </div>
