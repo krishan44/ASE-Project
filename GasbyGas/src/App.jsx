bde5bc9a42c9f4dbbf546dd2bf305bbf4c5f3600
Kavidu Krishan <kavindukrishan986@gmail.com>
2025-01-14 23:09:01 +0530

    Add react-router-dom for routing and update App component with routes for Overview, CustomerOrders, and CustomerTable

--- GasbyGas/src/App.jsx
+++ GasbyGas/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import Overview from "./Components/OutletManager/Overview";  
-import CustomerOrders from "./Components/OutletManager/customerOrders";  
-import CustomerTable from "./Components/OutletManager/Tables/customerTable";  
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Overview from "./Components/OutletManager/Overview";
+import CustomerOrders from "./Components/OutletManager/customerOrders";
+import CustomerTable from "./Components/OutletManager/Tables/customerTable";
 
 function App() {
-
   return (
-    <>
-      <CustomerTable/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Overview />} />
+        <Route path="/orders" element={<CustomerOrders />} />
+        <Route path="/customers" element={<CustomerTable />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
