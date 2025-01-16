c523aaa92a45d976f73ea95a2d1165eb94767ae5
krishan44 <124803438+krishan44@users.noreply.github.com>
2025-01-16 23:58:17 +0530

    Almost Done with the pop ups

--- GasbyGas/src/Components/OutletManager/Tables/addNew.jsx
+++ GasbyGas/src/Components/OutletManager/Tables/addNew.jsx
@@ -7,6 +7,7 @@ function addNew(){
     return(
         <Box sx={{
             width:'400px',
+            height:'600px',
             boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
             padding:'14px 14px',    
             borderRadius:'10px'    
@@ -16,7 +17,7 @@ function addNew(){
             </Typography>
             <Stack>
                 <Box sx={{marginTop:'20px'}}>
-                    <Typography sx={{fontSize:'14px', fontWeight:'400',fontFamily:'Poppins'}}>Token ID : </Typography>
+                    <Typography sx={{fontSize:'14px', fontWeight:'400',fontFamily:'Poppins'}}>Token ID : 121</Typography>
                 </Box>
                 <Box>
                     <Typography sx={{fontSize:'14px', fontWeight:'400',fontFamily:'Poppins',marginTop:'5px'}}>Customer Name</Typography>
@@ -52,7 +53,8 @@ function addNew(){
                                     width: "60px",
                                     fontFamily:'Poppins',
                                     fontSize:'12px',
-                                    marginTop:'5px'
+                                    marginTop:'5px',
+                                    textAlign:'center'
                                     },
                                 }}
                             />
@@ -71,7 +73,8 @@ function addNew(){
                                     width: "60px",
                                     fontFamily:'Poppins',
                                     fontSize:'12px',
-                                    marginTop:'5px'
+                                    marginTop:'5px',
+                                    textAlign:'center'
                                     },
                                 }}
                             />
@@ -90,7 +93,8 @@ function addNew(){
                                     width: "65px",
                                     fontFamily:'Poppins',
                                     fontSize:'12px',
-                                    marginTop:'5px'
+                                    marginTop:'5px',
+                                    textAlign:'center'
                                     },
                                 }}
                             />
@@ -98,9 +102,189 @@ function addNew(){
                         </Box>
                     </Box>  
                 </Box>
-                    
+                <Box>
+                <Typography sx={{fontSize:'14px', fontWeight:'400',fontFamily:'Poppins',marginTop:'10px'}}>Order Date</Typography>
+                    <Box>
+                       <TextField 
+                       type="date"
+                       InputLabelProps={{
+                        shrink: true, // Ensures the label does not overlap the input
+                      }}
+                      fullWidth
+                    sx={{
+                    maxWidth: '300',
+                    marginTop:'5px',
+                    '& .MuiInputBase-root': {
+                        borderRadius: '8px',
+                    },
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                        borderColor: '#ccc', 
+                        },
+                        '&:hover fieldset': {
+                        borderColor: '#1976d2',
+                        },
+                        '&.Mui-focused fieldset': {
+                        borderColor: '#1976d2', 
+                        },
+                    },
+                    height: "40px", 
+                            width: "280px",
+                    }}
+                    />
+                    </Box>
+                </Box>
+                <Box>
+                <Typography sx={{fontSize:'14px', fontWeight:'400',fontFamily:'Poppins',marginTop:'25px'}}>Tank Status</Typography>
+                <Box sx={{display:'flex', gap:'5px'}}>
+                        <Box sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                            <RemoveIcon/>
+                            <TextField
+                                id="outlined-basic"
+                                variant="outlined"
+                                placeholder='2.5Kg'
+                                sx={{
+                                    
+                                    "& .MuiInputBase-root": {
+                                    height: "40px", // Adjusts the container height
+                                    width: "60px",
+                                    fontFamily:'Poppins',
+                                    fontSize:'12px',
+                                    marginTop:'5px',
+                                    textAlign:'center'
+                                    },
+                                }}
+                            />
+                            <AddIcon/>
+                        </Box>
+                        <Box sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                            <RemoveIcon/>
+                            <TextField
+                                id="outlined-basic"
+                                variant="outlined"
+                                placeholder=' 5 Kg'
+                                sx={{
+                                    
+                                    "& .MuiInputBase-root": {
+                                    height: "40px", // Adjusts the container height
+                                    width: "60px",
+                                    fontFamily:'Poppins',
+                                    fontSize:'12px',
+                                    marginTop:'5px',
+                                    textAlign:'center'
+                                    },
+                                }}
+                            />
+                            <AddIcon/>
+                        </Box>
+                        <Box sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                            <RemoveIcon/>
+                            <TextField
+                                id="outlined-basic"
+                                variant="outlined"
+                                placeholder='12.5Kg'
+                                sx={{
+                                    
+                                    "& .MuiInputBase-root": {
+                                    height: "40px", // Adjusts the container height
+                                    width: "65px",
+                                    fontFamily:'Poppins',
+                                    fontSize:'12px',
+                                    marginTop:'5px',
+                                    textAlign:'center'
+                                    },
+                                }}
+                            />
+                            <AddIcon/>
+                        </Box>
+                    </Box> 
+                </Box>    
+                <Box>
+                    <Typography sx={{fontSize:'14px', fontWeight:'400',fontFamily:'Poppins',marginTop:'5px'}}>Contact Number</Typography>
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        
+                        sx={{
+                            
+                            "& .MuiInputBase-root": {
+                            height: "40px", // Adjusts the container height
+                            width: "280px",
+                            fontFamily:'Poppins',
+                            fontSize:'12px',
+                            marginTop:'5px'
+                            },
+                        }}
+                        />
+                </Box>
+                <Box>
+                    <Typography sx={{fontSize:'14px', fontWeight:'400',fontFamily:'Poppins',marginTop:'5px'}}>Email </Typography>
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        
+                        sx={{
+                            
+                            "& .MuiInputBase-root": {
+                            height: "40px", // Adjusts the container height
+                            width: "280px",
+                            fontFamily:'Poppins',
+                            fontSize:'12px',
+                            marginTop:'5px'
+                            },
+                        }}
+                        />
+                </Box>
             
             </Stack>
+            <Box sx={{display:'flex', justifyContent:'space-between', gap:'10px'}}>
+            <Typography
+                sx={{
+                    width: '100px',
+                    height: '40px',
+                    color: '#FFF',
+                    cursor: 'pointer',
+                    textAlign: 'center',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginTop: '10px',
+                    borderRadius:'5px',
+                    backgroundColor:'#D84040',
+                    '&:hover': {
+                    color: '#D84040',
+                    backgroundColor:'transparent', // Change color on hover
+                    border:'1px solid #D84040'
+                    },
+                }}
+                >
+                Cancel
+                </Typography>
+                
+
+                <Typography
+                sx={{
+                    width: '100px',
+                    height: '40px',
+                    color: '#FFF',
+                    cursor: 'pointer',
+                    textAlign: 'center',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginTop: '10px',
+                    borderRadius:'5px',
+                    backgroundColor:'#344CB7',
+                    '&:hover': {
+                    color: '#344CB7',
+                    backgroundColor:'transparent', // Change color on hover
+                    border:'1px solid #344CB7'
+                    },
+                }}
+                >
+                Save
+                </Typography>
+            </Box>
         </Box>
     )
 
