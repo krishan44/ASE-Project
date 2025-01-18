e2322d1dfefd4fca56fcab9e4944330880415eb0
Kavidu Krishan <kavindukrishan986@gmail.com>
2025-01-19 02:05:45 +0530

    Made some changes to the delete of the customer order

--- GasbyGas/src/Components/OutletManager/Notifications/delete.jsx
+++ GasbyGas/src/Components/OutletManager/Notifications/delete.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Box, Typography, Stack, TextField,Button } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Delete(){
 
@@ -11,7 +12,7 @@ function Delete(){
 
 
     return(
-        <Stack sx={{padding:'10px 12px'}}>
+        <Stack sx={{padding:'10px 12px', width:'800px' }}>
             <Typography sx={{
                 color:'#FB4141',
                 fontSize:'18px',
@@ -20,8 +21,9 @@ function Delete(){
             }}>
                 Delete an Order
             </Typography>
-            <Box sx={{display:'flex', alignItems:'center', gap:'10px'}}>
-                <Typography
+            <Box sx={{display:'flex', alignItems:'center',justifyContent:'space-between', gap:'10px', marginTop:'20px'}}>
+              <Box sx={{display:'flex', alignItems:'center', gap:'10px'}}>
+              <Typography
                  sx={
                     {
                          fontFamily:'poppins'
@@ -36,18 +38,51 @@ function Delete(){
                 fontFamily: 'Poppins',
                 fontSize: '12px',
                 marginTop: '5px',
-                borderRadius:'2px'
+                borderRadius:'5px'
               },
             }}
                 >
                 </TextField>
-                <Button variant="outlined" 
-                sx={{
-                    fontFamily:'poppins'
-                }}
-                > 
-                    Search</Button>
-
+                <Box sx={{
+                  width:'40px',
+                  height:'40px',
+                  borderRadius:'50%',
+                  display:'flex',
+                  alignItems:'center',
+                  justifyContent:'center',
+                  border:'1px solid #344CB7',
+                  backgroundColor:'#344CB7',
+                  cursor:'pointer'
+                }}>
+                <SearchIcon sx={{color:'#FFF'}}/>
+                </Box>
+              </Box>
+               <Box>
+                
+               </Box>
+                <Box sx={{
+          display:'flex',
+          justifyContent:'center'
+        , marginRight:'70px'
+        }}>
+          <Typography sx={{width:'100px',
+              height:'40px',
+              backgroundColor:'#D84040',
+              display:'flex',
+              justifyContent:'center',
+              alignItems:'center',
+              borderRadius:'5px',
+              color:'#FFF',
+              '&: hover': {
+                backgroundColor:'transparent',
+                color:'#D84040',
+                border:'1px solid #D84040',
+                cursor:'pointer'
+              }
+              }}>
+            Delete
+          </Typography>
+        </Box>
             </Box>
 
             {/* Customer Name and Ordered Date */}
@@ -145,6 +180,54 @@ function Delete(){
                 
             </Box>
 
+            {/* Email andContact Number*/}
+
+            <Box sx={{marginTop:'10px'}}>
+            <Box sx={{display:'flex', alignItems:'center', gap:'10px'}}>
+            <Typography sx={{
+                 fontFamily:'poppins',
+                 width:'140px'
+            }}>
+                   Email
+            </Typography>
+            <TextField sx={{
+              '& .MuiInputBase-root': {
+                height: '30px',
+                width: '200px',
+                fontFamily: 'Poppins',
+                fontSize: '12px',
+                marginTop: '5px',
+                borderRadius:'5px'
+              },
+            }}
+            disabled
+                >
+                </TextField>
+                <Typography sx={{
+                 fontFamily:'poppins',
+                    width:'140px'
+            }}>
+                    Contact Number
+            </Typography>
+            <TextField 
+            sx={{
+              '& .MuiInputBase-root': {
+                height: '30px',
+                width: '200px',
+                fontFamily: 'Poppins',
+                fontSize: '12px',
+                marginTop: '5px',
+                borderRadius:'5px'
+              },
+            }}
+            disabled
+                >
+                </TextField>
+                </Box>
+                
+            </Box>
+                
+       
         </Stack>
     )
 }
